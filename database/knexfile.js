ec9be0f150a2a410e185208a3c25fe81d200feae
Håkon Westh-Hansen <>
2019-09-17 17:52:02 +0200

    Added knex and initial databse migration

---
+++ database/knexfile.js
@@ -0,0 +1,14 @@
+require('load-environment')
+
+module.exports = {
+  development: {
+    client: 'pg',
+    connection: process.env.REGTIC_DATABASE_URL,
+    migrations: {
+      directory: 'migrations',
+    },
+    seeds: {
+      directory: 'seeds'
+    }
+  }
+}
