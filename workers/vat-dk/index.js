4e04e5d2ea41467e9d15cf0974f18a8c91be9927
Håkon Westh-Hansen <>
2019-10-10 20:44:53 +0200

    Fixed scrollAndParse

--- workers/vat-dk/index.js
+++ workers/vat-dk/index.js
@@ -1,101 +1,86 @@
 // dotenv handler
-require('load-environment')
+require("load-environment");
 
 // imports
-const axios = require('axios')
-const cvrParser = require('./cvr-parser')
-const dbHelper = require('./db-helper')
+const axios = require("axios");
+const cvrParser = require("./cvr-parser");
+const dbHelper = require("./db-helper");
 
 // configurations
 const cvrApi = axios.create({
   baseURL: process.env.CVR_BASE_URL,
   timeout: 5000,
   headers: {
-    'Content-Type': 'application/json'
+    "Content-Type": "application/json"
   },
   auth: {
     username: process.env.CVR_USER,
     password: process.env.CVR_PASS
   }
-})
+});
 
 const query = {
   size: 100,
-	_source: [
-    'Vrvirksomhed.cvrNummer',
-    'Vrvirksomhed.virksomhedMetadata.nyesteNavn.navn',
-    'Vrvirksomhed.virksomhedMetadata.nyesteBeliggenhedsadresse',
-    'Vrvirksomhed.virksomhedMetadata.stiftelsesDato',
-    'Vrvirksomhed.deltagerRelation.deltager',
-    'Vrvirksomhed.deltagerRelation.organisationer',
+  _source: [
+    "Vrvirksomhed.cvrNummer",
+    "Vrvirksomhed.virksomhedMetadata.nyesteNavn.navn",
+    "Vrvirksomhed.virksomhedMetadata.nyesteBeliggenhedsadresse",
+    "Vrvirksomhed.virksomhedMetadata.stiftelsesDato",
+    "Vrvirksomhed.deltagerRelation.deltager",
+    "Vrvirksomhed.deltagerRelation.organisationer"
   ],
-	query: {
+  query: {
     bool: {
-      must: [
-        { exists: { field: 'Vrvirksomhed.cvrNummer' } }
-      ],
+      must: [{ exists: { field: "Vrvirksomhed.cvrNummer" } }],
       must_not: [
-        { exists: { field: 'Vrvirksomhed.livsforloeb.periode.gyldigTil' } }
+        { exists: { field: "Vrvirksomhed.livsforloeb.periode.gyldigTil" } }
       ]
     }
   }
-}
+};
 
-async function scrollRequest (body, config = {}) {
-  const scrollTimeout = config.scrollTimeout
-    ? config.scrollTimeout
-    : '1m'
+async function scrollRequest(body, config = {}) {
+  const scrollTimeout = config.scrollTimeout ? config.scrollTimeout : "1m";
   const endpoint = config.scrollId
-    ? '_search/scroll'
-    : `cvr-permanent/_search?scroll=${scrollTimeout}`
-  return cvrApi.post(endpoint, body)
+    ? "_search/scroll"
+    : `cvr-permanent/_search?scroll=${scrollTimeout}`;
+  return cvrApi.post(endpoint, body);
 }
 
 async function parseAndSaveResponse(response) {
-  const hitList = response.data.hits.hits
+  const hitList = response.data.hits.hits;
   hitList.forEach(async hit => {
-    const company = cvrParser.parse(hit)
+    const company = cvrParser.parse(hit);
 
-    const insertCompanyResult = await dbHelper.insertCompany(company)
-    const companyId = insertCompanyResult.rows[0].id
+    const insertCompanyResult = await dbHelper.insertCompany(company);
+    const companyId = insertCompanyResult.rows[0].id;
 
     company.persons.forEach(async person => {
-      const insertPersonResult = await dbHelper.insertPerson(person)
-      const personId = insertPersonResult.rows[0].id
-      await dbHelper.insertCompanyToPerson(companyId, personId)
-    })
+      const insertPersonResult = await dbHelper.insertPerson(person);
+      const personId = insertPersonResult.rows[0].id;
+      await dbHelper.insertCompanyToPerson(companyId, personId);
+    });
 
     company.motherCompanies.forEach(async motherCompany => {
-      const motherCompanyInsert = await dbHelper.insertCompany(motherCompany)
-      const motherCompanyId = motherCompanyInsert.rows[0].id
-      await dbHelper.insertCompanyToCompany(motherCompanyId, companyId)
-    })
-  })
+      const motherCompanyInsert = await dbHelper.insertCompany(motherCompany);
+      const motherCompanyId = motherCompanyInsert.rows[0].id;
+      await dbHelper.insertCompanyToCompany(motherCompanyId, companyId);
+    });
+  });
 }
 
-module.exports.scrollAndParse = async (event, context) => {
-  const scrollRequestResponse = await scrollRequest(query, { scrollId: !!event.body.scrollId })
-  const scrollIdFromResponse = scrollRequestResponse.data._scroll_id
-
-  await parseAndSaveResponse(scrollRequestResponse)
-
-  return { statusCode: 200, body: { scrollId: scrollIdFromResponse } }
-}
-
-/*
-async function scrollTest () {
+export async function scrollAndParse(event, context) {
   try {
-    const initialRequestResponse = await scrollRequest(query, { initialRequest: true })
-    const subsequentRequestResponse = await scrollRequest({ scroll: '1m', scroll_id: initialRequestResponse.data._scroll_id })
+    const scrollRequestResponse = await scrollRequest(query, {
+      scrollId: event.body.scrollId
+    });
+    const scrollIdFromResponse = scrollRequestResponse.data._scroll_id;
 
-    await parseAndSaveResponse(initialRequestResponse)
-    await parseAndSaveResponse(subsequentRequestResponse)
-  }
-  catch (error) {
-    console.log('### ERROR INCOMING ###')
-    console.log(error)
+    await parseAndSaveResponse(scrollRequestResponse);
+
+    return { statusCode: 200, body: { scrollId: scrollIdFromResponse } };
+  } catch (error) {
+    console.log(error);
+    return { statusCode: 500 };
   }
 }
-
-scrollTest()
-*/
