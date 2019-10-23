c1fdbb5175d95f8d77cc46d32cea5ed41205f6b7
Alexandros Dorodoulis <aldor94@gmail.com>
2019-10-23 22:50:14 +0200

    feat(vat-dk): Add vectorization to the name

--- workers/vat-dk/db-helper.js
+++ workers/vat-dk/db-helper.js
@@ -11,8 +11,8 @@ async function insertCompany(company) {
   try {
     const result = await client.query(
       `
-      INSERT INTO companies (name, address, vat, starting_date, country_code)
-      VALUES ($1, $2, $3, $4, $5)
+      INSERT INTO companies(name, name_vector, address, vat, starting_date, country_code)
+      VALUES ($1, to_tsvector($1) , $2, $3, $4, $5)
       ON CONFLICT (vat) DO UPDATE SET vat = $3
       RETURNING id`,
       [
