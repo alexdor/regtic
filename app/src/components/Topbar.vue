6406f7783ca3be1dcb03fe64bcbc6f1455424462
Andreas <bloxxer9001@gmail.com>
2019-10-24 18:04:01 +0200

    Topbar is now a component used within router views.

---
+++ app/src/components/Topbar.vue
@@ -0,0 +1,41 @@
+<template>
+  <el-header>
+    <el-row>
+      <el-col :span="12">
+        <el-page-header @back="goBack"
+                        :content="title"></el-page-header>
+      </el-col>
+      <el-col :span="12" class="align-right">
+        <div class="vertical-center">
+          <i class="el-icon-bell"></i>
+          <el-divider direction="vertical"></el-divider>
+          <el-avatar shape="circle"
+                     :size="32"
+                     src="https://cube.elemecdn.com/3/7c/3ea6beec64369c2642b92c6726f1epng.png"></el-avatar>
+        </div>
+      </el-col>
+    </el-row>
+  </el-header>
+</template>
+<script>
+  export default {
+    data() {
+      return {
+        title: "Title"
+      }
+    },
+    props: ["title"],
+    created() {
+      console.log(this);
+      document.title = this.title;
+    }
+  };
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+
+  .el-header {
+    background-color: #fff;
+  }
+</style>
--- app/src/views/CheckCompany.vue
+++ app/src/views/CheckCompany.vue
@@ -1,15 +1,30 @@
 <template>
-  <CheckCompany />
+  <div class="full-height">
+    <Topbar title="Check company" />
+    <el-main>
+      <CheckCompany />
+    </el-main>
+  </div>
 </template>
 
 <script lang="ts">
-import { Component, Vue } from "vue-property-decorator";
-import CheckCompany from "@/components/CheckCompany.vue"; // @ is an alias to /src
+  import { Component, Vue } from "vue-property-decorator";
+  import CheckCompany from "@/components/CheckCompany.vue"; // @ is an alias to /src
+  import Topbar from "@/components/Topbar.vue"; // @ is an alias to /src
 
-@Component({
-  components: {
-    CheckCompany
-  }
-})
-export default class Home extends Vue {}
+  @Component({
+    components: {
+      CheckCompany,
+      Topbar
+    }
+  })
+  export default class Home extends Vue { }
 </script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+.full-height {
+  height: 100%;
+  background-color: #fafafa;
+}
+</style>
