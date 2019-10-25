a05d144aaa3b053f7e6858bac1141e32b3f1bc34
Andreas <Andreas@10.16.168.88>
2019-10-25 14:50:53 +0200

    Notification items are now a sub-component.

---
+++ app/src/components/NotificationItem.vue
@@ -0,0 +1,60 @@
+<template>
+  <el-row>
+    <el-col :span="3" class="center-icon">
+      <i
+        :class="
+          'el-notification-icon el-icon-' +
+            notification_types[data.type].icon +
+            ' el-color-' +
+            notification_types[data.type].color
+        "
+      ></i>
+    </el-col>
+    <el-col :span="21">
+      <p v-html="data.text"></p>
+    </el-col>
+  </el-row>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      notification_types: {
+        status: { color: "primary", icon: "document" },
+        add: { color: "success", icon: "plus" },
+        warn: { color: "warning", icon: "alarm-clock" }
+      }
+    };
+  },
+  props: {
+    data: {
+      type: Object
+    }
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+.el-color-primary {
+  color: #409eff;
+}
+
+.el-color-success {
+  color: #67c23a;
+}
+
+.el-color-warning {
+  color: #e6a23c;
+}
+
+.el-notification-icon {
+  font-size: 24px;
+}
+
+.el-row {
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+}
+</style>
