6dbea4f5f29f4b9b64af95ebf540613309730f3d
Alexandros Dorodoulis <aldor94@gmail.com>
2019-11-02 10:52:54 +0100

    feat(db): Upgrade to pg12 and add virtual columns for vectors
    Postgres 12 supports virtual columns, so we use them for our vectors
    This commit also improves the vector columns by using the simple dictionary instead of the english one, this is done because we vectorize names and not words
    Finally this commit also adds vectors for persons, bad_persons and bad_persons_aliases

---
+++ database/migrations/20191102091827_add-virtual-columns-for-search.js
@@ -0,0 +1,30 @@
+exports.up = function(knex) {
+  return knex.schema.raw(`
+    ALTER TABLE companies DROP IF EXISTS name_vector;
+    ALTER TABLE companies ADD name_vector tsvector
+      generated always as (
+        to_tsvector('simple', name)
+      ) stored;
+    ALTER TABLE persons ADD name_vector tsvector
+      generated always as (
+        to_tsvector('simple', first_name || ' ' || last_name)
+      ) stored;
+    ALTER TABLE bad_persons ADD name_vector tsvector
+      generated always as (
+        to_tsvector('simple', full_name)
+      ) stored;
+    ALTER TABLE bad_persons_aliases ADD name_vector tsvector
+      generated always as (
+        to_tsvector('simple', full_name)
+      ) stored;
+  `);
+};
+
+exports.down = function(knex) {
+  return knex.schema.raw(`
+    ALTER TABLE companies DROP IF EXISTS name_vector;
+    ALTER TABLE persons DROP IF EXISTS name_vector;
+    ALTER TABLE bad_persons DROP IF EXISTS name_vector;
+    ALTER TABLE bad_persons_aliases DROP IF EXISTS name_vector;
+  `);
+};
