fb59e1182f09e185513dc4989328d3c9091e2e45
Alexandros Dorodoulis <aldor94@gmail.com>
2019-11-03 13:48:12 +0100

    refactor(api): Use db's function instead of doing it manually

--- api/db/find_companies.go
+++ api/db/find_companies.go
@@ -2,20 +2,15 @@ package db
 
 import (
 	"context"
-	"strings"
 
 	"github.com/alexdor/regtic/api/models"
 	"github.com/volatiletech/sqlboiler/queries/qm"
 )
 
 func FindCompany(ctx context.Context, name string) ([]*models.Company, error) {
-	var nameQuery strings.Builder
-	for i, value := range strings.Split(name, " ") {
-		if i != 0 {
-			nameQuery.WriteString(" & ")
-		}
-		nameQuery.WriteString(value)
-	}
-	query := qm.Where("name_vector @@ to_tsquery(?)", nameQuery.String())
-	return models.Companies(query).All(ctx, DB)
+	return models.Companies(
+		qm.Where(
+			"name_vector @@ plainto_tsquery('simple', ?)", name,
+		),
+	).All(ctx, DB)
 }
