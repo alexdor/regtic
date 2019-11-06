8c198c6298b053b751d15da59afc6706acddcdeb
Alexandros Dorodoulis <aldor94@gmail.com>
2019-11-06 09:56:37 +0100

    feat(db): Turn bad_person type to enum, add not null constrain

---
+++ database/migrations/20191106092459_turn-bad-person-types-to-enum.js
@@ -0,0 +1,13 @@
+exports.up = function(knex) {
+  return knex.schema.raw(`
+    CREATE TYPE BAD_PERSON_TYPE AS ENUM ('PEP', 'SANCTION');
+    ALTER TABLE bad_persons ALTER COLUMN type TYPE BAD_PERSON_TYPE using type::BAD_PERSON_TYPE;
+  `);
+};
+
+exports.down = function(knex) {
+  return knex.schema.raw(`
+    ALTER TABLE bad_persons ALTER COLUMN type TYPE TEXT;
+    DROP TYPE IF EXISTS BAD_PERSON_TYPE;
+  `);
+};
---
+++ database/migrations/20191106093438_turn-bad-person-types-to-not-nulls.js
@@ -0,0 +1,11 @@
+exports.up = function(knex) {
+  return knex.schema.raw(`
+    ALTER TABLE bad_persons ALTER COLUMN type SET NOT NULL;
+  `);
+};
+
+exports.down = function(knex) {
+  return knex.schema.raw(`
+    ALTER TABLE bad_persons ALTER COLUMN type DROP NOT NUL;
+  `);
+};
