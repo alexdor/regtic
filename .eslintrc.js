e2de0bef24aafb7ef6cdf3b26635d104021f96b2
Alexandros Dorodoulis <aldor94@gmail.com>
2019-11-08 10:27:19 +0100

    chore(tooling): Make eslint stricter, autoinstall packages

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,14 +4,28 @@ module.exports = {
     es6: true,
     node: true
   },
-  extends: ["standard", "prettier", "plugin:prettier/recommended", "plugin:vue/essential",
-  "@vue/prettier", "@vue/typescript"],
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/eslint-recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:@typescript-eslint/recommended-requiring-type-checking",
+    "prettier",
+    "prettier/@typescript-eslint",
+    "plugin:vue/recommended",
+    "@vue/prettier",
+    "@vue/typescript"
+  ],
+  plugins: ["@typescript-eslint"],
   globals: {
     Atomics: "readonly",
     SharedArrayBuffer: "readonly"
   },
+  rules: {
+    "@typescript-eslint/explicit-function-return-type": "off"
+  },
   parserOptions: {
     ecmaVersion: 2018,
+    project: "tsconfig.json",
     parser: "@typescript-eslint/parser"
   }
 };
