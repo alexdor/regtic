d3d0ab23044c24f3c874e485e2f6a66442e661d4
Alexandros Dorodoulis <aldor94@gmail.com>
2019-11-16 14:45:42 +0100

    feat(db): Move address field to external table

--- database/migrations/20191112113640_add-bad-company-and-indexes.js
+++ database/migrations/20191112113640_add-bad-company-and-indexes.js
@@ -1,34 +1,67 @@
+const defaultFieldsForTable = `id UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
+  updated_at timestamp DEFAULT now() NOT NULL,
+  created_at timestamp DEFAULT now() NOT NULL`;
+
+const uniqueRel = (tableName, columns) =>
+  `CREATE UNIQUE INDEX ${tableName}_${columns
+    .split(", ")
+    .join("_")}_uniqueness ON ${tableName} (${columns});`;
+
+const countryCodeType = `varchar(2) default 'ZZ' NOT NULL`;
+const addressInfo = `country_code ${countryCodeType},
+  street TEXT,
+  city TEXT,
+  zip_code TEXT,
+  region TEXT,
+  place TEXT,
+  po_box  TEXT`;
+
+const foreignKeyToID = (keyName, table) =>
+  `${keyName} UUID,
+ FOREIGN KEY (${keyName}) REFERENCES ${table} (id)`;
+
+const citizenShip = `citizenship_country_code varchar(2)[] default '{ZZ}' NOT NULL`;
+
 exports.up = function(knex) {
   return knex.schema.raw(`
+    CREATE TYPE ADDRESS_TYPE AS ENUM ('birthplace', 'address');
+
     CREATE TABLE bad_companies (
-    id UUID DEFAULT uuid_generate_v4 () PRIMARY KEY,
-    name TEXT NOT NULL,
-    name_vector tsvector
-    generated always as (
-      to_tsvector('simple', name)
-    ) stored,
-    address TEXT,
-    source TEXT NOT NULL,
-    country_code varchar(2) default 'ZZ' NOT NULL,
-    updated_at timestamp DEFAULT now() NOT NULL,
-    created_at timestamp DEFAULT now() NOT NULL,
-    type BAD_PERSON_TYPE NOT NULL
+      ${defaultFieldsForTable},
+      name TEXT NOT NULL,
+      name_vector tsvector
+      generated always as (
+        to_tsvector('simple', name)
+      ) stored,
+      address TEXT,
+      source TEXT NOT NULL,
+      citizenship_region TEXT,
+      type BAD_PERSON_TYPE NOT NULL,
+      ${citizenShip}
     );
 
     CREATE TABLE bad_companies_aliases (
-    id UUID DEFAULT uuid_generate_v4 () PRIMARY KEY,
-    company_id UUID,
-    name TEXT NOT NULL,
-    name_vector tsvector
-    generated always as (
-      to_tsvector('simple', name)
-    ) stored,
-    address TEXT,
-    source TEXT NOT NULL,
-    country_code varchar(2) default 'ZZ' NOT NULL,
-    updated_at timestamp DEFAULT now() NOT NULL,
-    created_at timestamp DEFAULT now() NOT NULL,
-    type BAD_PERSON_TYPE NOT NULL
+      ${defaultFieldsForTable},
+      ${foreignKeyToID("bad_company_id", "bad_companies")},
+      name TEXT NOT NULL,
+      name_vector tsvector
+      generated always as (
+        to_tsvector('simple', name)
+        ) stored,
+      type BAD_PERSON_TYPE NOT NULL
+    );
+
+    CREATE TABLE bad_companies_addresses (
+      ${defaultFieldsForTable},
+      ${foreignKeyToID("bad_company_id", "bad_companies")},
+      ${addressInfo}
+    );
+
+    CREATE TABLE bad_persons_addresses (
+      ${defaultFieldsForTable},
+      ${foreignKeyToID("bad_person_id", "bad_persons")},
+      ${addressInfo},
+      type ADDRESS_TYPE NOT NULL
     );
 
     ALTER TABLE companies ALTER COLUMN country_code SET DEFAULT 'ZZ';ALTER TABLE companies ALTER COLUMN country_code SET NOT NULL;
@@ -36,14 +69,26 @@ exports.up = function(knex) {
     ALTER TABLE persons ALTER COLUMN country_code SET DEFAULT 'ZZ';
     ALTER TABLE persons ALTER COLUMN country_code SET NOT NULL;
 
-    ALTER TABLE bad_companies_aliases ADD FOREIGN KEY (company_id) REFERENCES companies (id);
-    ALTER TABLE bad_persons ADD country_code varchar(2) DEFAULT 'ZZ' NOT NULL;
+    ALTER TABLE bad_persons DROP IF EXISTS country_code;
+    ALTER TABLE bad_persons ADD ${citizenShip};
 
-    CREATE UNIQUE INDEX bad_persons_uniqueness ON bad_persons (name_vector, address, country_code, type);
-    CREATE UNIQUE INDEX bad_companies_uniqueness ON bad_companies (name_vector, address, country_code, type);
-    CREATE UNIQUE INDEX bad_companies_aliases_uniqueness ON bad_companies_aliases (company_id, name);
-    CREATE UNIQUE INDEX bad_persons_aliases_uniqueness ON bad_persons_aliases (bad_person_id, full_name);
-  `);
+    ${uniqueRel(
+      "bad_persons",
+      "name_vector, address, citizenship_country_code, type"
+    )}
+    ${uniqueRel(
+      "bad_persons_addresses",
+      "bad_person_id, country_code, street, city, zip_code, region, place, po_box"
+    )}
+    ${uniqueRel("bad_persons_aliases", "bad_person_id, full_name")}
+
+    ${uniqueRel("bad_companies", "name_vector, address, type")}
+    ${uniqueRel("bad_companies_aliases", "bad_company_id, name")}
+    ${uniqueRel(
+      "bad_companies_addresses",
+      "bad_company_id, country_code, street, city, zip_code, region, place, po_box"
+    )}
+     `);
 };
 
 exports.down = function(knex) {
@@ -52,8 +97,12 @@ exports.down = function(knex) {
     DROP INDEX IF EXISTS bad_persons_aliases_uniqueness;
     DROP INDEX IF EXISTS bad_persons_uniqueness;
     DROP INDEX IF EXISTS bad_companies_uniqueness;
-    ALTER TABLE bad_persons DROP country_code;
-    DROP TABLE bad_companies;
+    ALTER TABLE bad_persons DROP IF EXISTS citizenship_country_code;
     DROP TABLE bad_companies_aliases;
+    DROP TABLE bad_companies_addresses;
+    DROP TABLE bad_persons_addresses;
+    DROP TABLE bad_companies;
+    DROP INDEX IF EXISTS     bad_persons_aliases_bad_person_id_full_name_uniqueness;
+    DROP TYPE IF EXISTS ADDRESS_TYPE;
   `);
 };
