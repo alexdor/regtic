a4ab17fef40f3d2823fbe9aea2b430ea1417203c
Håkon Westh-Hansen <>
2019-12-14 14:03:06 +0100

    refactor: 💡 moved the key declarations inside create-statement

--- database/migrations/20191214131317_add-relationships.js
+++ database/migrations/20191214131317_add-relationships.js
@@ -2,19 +2,19 @@ exports.up = function(knex) {
   return knex.schema.raw(`
     CREATE TABLE bad_person_to_person(
       bad_person_id UUID NOT NULL,
-      person_id UUID NOT NULL
+      person_id UUID NOT NULL,
+      FOREIGN KEY (person_id) REFERENCES persons (id),
+      FOREIGN KEY (bad_person_id) REFERENCES bad_persons (id),
+      PRIMARY KEY (bad_person_id,person_id)
     );
-    ALTER TABLE bad_person_to_person ADD FOREIGN KEY (person_id) REFERENCES persons (id);
-    ALTER TABLE bad_person_to_person ADD FOREIGN KEY (bad_person_id) REFERENCES bad_persons (id);
-    ALTER TABLE bad_person_to_person ADD primary key(bad_person_id,person_id);
 
     CREATE TABLE bad_company_to_company(
       bad_company_id UUID NOT NULL,
-      company_id UUID NOT NULL
+      company_id UUID NOT NULL,
+      FOREIGN KEY (company_id) REFERENCES companies (id),
+      FOREIGN KEY (bad_company_id) REFERENCES bad_companies (id),
+      PRIMARY KEY (bad_company_id,company_id)
     );
-    ALTER TABLE bad_company_to_company ADD FOREIGN KEY (company_id) REFERENCES companies (id);
-    ALTER TABLE bad_company_to_company ADD FOREIGN KEY (bad_company_id) REFERENCES bad_companies (id);
-    ALTER TABLE bad_company_to_company ADD primary key(bad_company_id,company_id);
 
     ALTER TABLE company_to_company ADD voting_rights REAL NOT NULL DEFAULT 0.0;
     ALTER TABLE company_to_person ADD voting_rights REAL NOT NULL DEFAULT 0.0;
