5ce4355a606601b60f5d9cbb5286509c4f58a9c1
Alexandros Dorodoulis <aldor94@gmail.com>
2019-12-15 20:24:59 +0100

    refactor(api): Move things around

--- api/db/queries.go
+++ api/db/queries.go
@@ -6,3 +6,5 @@ var badCompanyOrderBy = models.BadCompanyColumns.Type + " desc"
 var badPersonWhereClause = models.BadPersonColumns.NameVector + " @@ plainto_tsquery('simple', ? )"
 var badPersonSelect = models.BadPersonColumns.Type + ", " + models.BadPersonColumns.Source + ", ts_rank(" + models.BadPersonColumns.NameVector + ", plainto_tsquery('simple', $1)) as rank"
 var badPersonOrderBy = "rank desc, " + models.BadPersonColumns.Type + " desc"
+
+var findCompanyWhereClause = models.CompanyColumns.NameVector + " @@ plainto_tsquery('simple', ?)"
