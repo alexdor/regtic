8412612c0490d0286305b71bb3045354b265a6dc
Alexandros Dorodoulis <aldor94@gmail.com>
2019-12-16 13:44:55 +0100

    fix(api): Only show directly related peoples/companies

--- api/db/queries.go
+++ api/db/queries.go
@@ -1,10 +1,23 @@
 package db
 
-import "github.com/alexdor/regtic/api/models"
+import (
+	"github.com/alexdor/regtic/api/models"
+	"github.com/volatiletech/sqlboiler/queries/qm"
+	"github.com/volatiletech/sqlboiler/types"
+)
 
-var badCompanyOrderBy = models.BadCompanyColumns.Type + " desc"
-var badPersonWhereClause = models.BadPersonColumns.NameVector + " @@ plainto_tsquery('simple', ? )"
-var badPersonSelect = models.BadPersonColumns.Type + ", " + models.BadPersonColumns.Source + ", ts_rank(" + models.BadPersonColumns.NameVector + ", plainto_tsquery('simple', $1)) as rank"
-var badPersonOrderBy = "rank desc, " + models.BadPersonColumns.Type + " desc"
+var (
+	badCompanyOrderBy    = models.BadCompanyColumns.Type + " desc"
+	badPersonWhereClause = models.BadPersonColumns.NameVector + " @@ plainto_tsquery('simple', ? )"
+	badPersonSelect      = models.BadPersonColumns.Type + ", " + models.BadPersonColumns.Source + ", ts_rank(" + models.BadPersonColumns.NameVector + ", plainto_tsquery('simple', $1)) as rank"
+	badPersonOrderBy     = "rank desc, " + models.BadPersonColumns.Type + " desc"
 
-var findCompanyWhereClause = models.CompanyColumns.NameVector + " @@ plainto_tsquery('simple', ?)"
+	findCompanyWhereClause = models.CompanyColumns.NameVector + " @@ plainto_tsquery('simple', ?)"
+	companyToPeopleFilter  = qm.AndIn(
+		models.CompanyToPersonColumns.Relations+" && ?",
+		types.StringArray{"ultimate beneficial owner", "legal owner", "founder"})
+
+	companyToCompanyFilter = qm.AndIn(
+		models.CompanyToCompanyColumns.Relations+" && ?",
+		types.StringArray{"ultimate beneficial owner", "legal owner", "founder"})
+)
