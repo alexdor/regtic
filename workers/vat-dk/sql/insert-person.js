679b795f9d4bb50e1c18dd4fc013f310689749fe
Alexandros Dorodoulis <aldor94@gmail.com>
2019-12-17 10:35:09 +0100

    feat(cvr): Split up address, improve null handling

--- workers/vat-dk/sql/insert-person.js
+++ workers/vat-dk/sql/insert-person.js
@@ -1,15 +1,17 @@
 module.exports = `
 INSERT INTO
-  persons (first_name, last_name, country_code, address)
+  persons (first_name, last_name, country_code, zip_code, region, street, city)
 VALUES
-  ($1, $2, $3, $4)
-ON CONFLICT
-  (first_name, last_name, country_code, address)
+  ($1, $2, $3, $4, $5, $6, $7)
+ON CONFLICT ON CONSTRAINT persons_unique_name_address
 DO UPDATE SET
   first_name = $1,
   last_name = $2,
   country_code = $3,
-  address = $4
+  zip_code = $4,
+  region = $5,
+  street = $6,
+  city = $7
 RETURNING
   id
 `;
